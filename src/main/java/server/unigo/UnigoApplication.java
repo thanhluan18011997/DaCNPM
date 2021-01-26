7ebd7b8e3683dd009072a2bbbacd335ad23c83e5
thanhluan18011997 <thanhluan18011997@gmail.com>
2021-01-26 19:46:44 +0700

    Create base code

---
+++ src/main/java/server/unigo/UnigoApplication.java
@@ -0,0 +1,13 @@
+package server.unigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnigoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/server/unigo/UnigoApplicationTests.java
@@ -0,0 +1,13 @@
+package server.unigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
